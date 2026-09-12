0086b426c18c617de11b004e8a00e56344ec33d6
Adonis-Rey <adonisreyes222@gmail.com>
2026-09-11 22:32:18 -0400

    Complete Assignment 1

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+// Adonis Reyes
+// "Hello World" Assignment 1
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "Hello, world!" << endl;
+
+    string name;
+    cout << "What's your name? ";
+    cin >> name;
+
+    cout << "Well, nice to meet you, " << name << "!";
+
+    return 0;
+}
